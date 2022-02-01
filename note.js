4837a35cc363a04f205648d96dba847800ec7dd9
Israel Adefidipe <israeladefidipe@gmail.com>
2022-02-01 21:17:35 +0100

    initial commit

---
+++ note.js
@@ -0,0 +1,16 @@
+// Node.js is a javascript runtime built on google's open-source v8 javascript engine
+// Javascript runtime is like a container or an environment where code written in Js can be run
+/**
+ * Nodejs pros:
+ * Single-threaded, based on event driven, non-blockin I/O model
+ * Perfect for building FASt and SCALLABLE data-intensive app
+ * 
+ * Use of Node.js
+ * API with database behind it (prefarably NoSQL)
+ * Data Streaming (like YoiTube and Netflix)
+ * Real-time chat application
+ * Server-side web applications 
+ * 
+ * DONT USE:
+ * Dont use with applications the need heavy server-side processing (CPU-intensive), heavy video compressions, image manipulations
+ */
